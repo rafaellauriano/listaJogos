c14ca2f837a5d80df0675b824266e0f951c9cd83
Rafael Silva Lauriano <rsilvalauriano@gmail.com>
2024-11-16 07:09:07 -0300

    Projeto criado

---
+++ src/test/java/com/rafaellauriano/listaJogos/ListaJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaellauriano.listaJogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
